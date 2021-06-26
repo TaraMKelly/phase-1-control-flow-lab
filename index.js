1f47c6dad215c46b2eef25cae4187720f8bf0881
TaraMKelly <tara.kelly16@gmail.com>
2021-06-26 10:41:26 -0400

    commit tree 8da43313f8d141b30ad8fe1543386af1f6fe8a8e

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(numberOfFeet){
+  if (numberOfFeet <= 400) return "This one is on me!";
+  else if (numberOfFeet > 2500) return "No can do."
+  else if (numberOfFeet > 2000) return "I will gladly take your thirty bucks.";
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(destination){
+  const message = destination === "NYC" ? "Ok, sounds good." : "No go.";
+  return message;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+      break;
+    case "not as generous":
+      return "Thank you.";
+      break;
+    default:
+      return "Bye."
+  }
 }
